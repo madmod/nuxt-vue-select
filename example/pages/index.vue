473480bd415ba6cb9a6c34de2875deb6001ca004
John Wells <john@daffinity.com>
2019-09-17 12:19:01 -0600

    feat: add plugin and tests

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    Works!
+    <h1>Vue Select</h1>
+    <v-select :options="options" />
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      options: [
+        'foo',
+        'bar',
+        'baz'
+      ]
+    }
+  }
 }
 </script>
